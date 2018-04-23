094c9d3b3ec49df0e42d3a3e86d60cdf5f99348d
Travis <travishn@uci.edu>
2018-04-23 15:38:26 -0700

    Created employee class and manager subclass

---
+++ Class_Inheritance/employee.rb
@@ -0,0 +1,49 @@
+class Employee
+  attr_reader :name, :title, :salary, :boss
+  
+  def initialize(name, title, salary, boss = nil)
+    @name = name
+    @title = title
+    @salary = salary
+    @boss = boss
+  end
+  
+  def boss=(name)
+    @boss = name
+    name.add_employee(self) unless boss.nil?
+  end
+  
+  def bonus(multiplier)
+    if self.class == Employee
+      salary * multiplier
+    else
+      employee_salary = employees.map(&:salary).inject(:+)
+      employee_salary * multiplier
+    end
+  end
+end
+
+class Manager < Employee
+  attr_reader :employees
+  
+  def initialize(name, title, salary)
+    @employees = []
+    super(name, title, salary, boss)
+  end
+  
+  def add_employee(name)
+    @employees << name
+  end
+  
+  def boss=(name)
+    super(name)
+    
+    employees.each do |employee|
+      name.add_employee(employee)
+    end
+    
+    name.name
+  end
+end
+
+  
